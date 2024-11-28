6dc9e6dccf430273216d1cfde127d15058877185
Shailender Singh <geekshailender@gmail.com>
2024-11-28 16:13:13 -0500

    details added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
-export const routes: Routes = [];
+export const routesConfig: Routes = [
+    { 
+        path: '', 
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Details Page'
+    }
+];
