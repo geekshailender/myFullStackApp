e7bf2aca0453031f16f924b6cff4892bd7f2d74f
Shailender Singh <geekshailender@gmail.com>
2024-11-28 21:08:01 -0500

    Final Commit

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,28 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HousingLocation } from './housinglocation';
-
+import {Injectable} from '@angular/core';
+import {HousingLocation} from './housinglocation';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HousingService {
-
-  url = "http://localhost:3000/locations"
-
-  constructor() { }
-
-  async getAllHousingLocationList() : Promise<HousingLocation[]> {
+  url = 'http://localhost:3000/locations';
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
     const data = await fetch(this.url);
-    return await data.json() ?? [];
+    return (await data.json()) ?? [];
   }
-
-  async getHousingLocationById(id: Number): Promise<HousingLocation | undefined> {
-     const data = await fetch(`${this.url}/${id}`);
-     return await data.json() ?? {};
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
   }
-
   submitApplication(firstName: string, lastName: string, email: string) {
-    console.log(
-      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
-    );
+    // tslint:disable-next-line
+    console.log(firstName, lastName, email);
   }
 }
